72b5c13028aaed9c90fba55f7f11d68203f1e07a
Hy <hyhuynh222@gmail.com>
2024-06-24 13:11:55 -0700

    Added Switch case

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -9,8 +9,19 @@ import { AmaJohnService } from '../demolist.service';
 export class DemoComponent {
   title = "List of Items"
   items;
+  category:number = 0;
 
   constructor(srv: AmaJohnService){
-    this.items = srv.getItems();
+    switch(this.category){
+      case 0:
+        this.items = srv.getItemsTech();
+        break;
+      case 1:
+        this.items = srv.getItemsHome();
+        break;
+      case 2:
+        this.items = srv.getItemsSupply();
+        break;
+    };
   }
 }
--- src/app/demolist.service.ts
+++ src/app/demolist.service.ts
@@ -1,12 +1,24 @@
 export class AmaJohnService {
-    getItems() {
+    getItemsTech() {
         return [
             {name:"Laptop", desc: "Gaming laptop with 4060 Ti", price: 799.99},
+            {name:"X-box Controller", desc: "Gaming controller made for XBox and PC", price: 69.99},
+            {name:"Iphone Carbon Fiber Case", desc: "Won't break trust", price: 56.99},
+        ]
+    }
+    getItemsHome() {
+        return [
             {name:"Foam Pillow", desc: "High-end shredded memory foam and microfiber pillow", 
                 price: 26.99},
-            {name:"X-box Controller", desc: "Gaming controller made for XBox and PC", price: 69.99},
+            {name:"Bed Sheet", desc: "Silky bed sheet made for max comfort", 
+                price: 44.99},
+            {name:"Dish 10 Set", desc: "Ceramic dishes", 
+                price: 16.99},
+        ]
+    }
+    getItemsSupply() {
+        return [
             {name:"Pencil", desc: "John Wick's Pencil...", price: 10000},
-            {name:"Iphone Carbon Fiber Case", desc: "Won't break trust", price: 56.99},
         ]
     }
 }
